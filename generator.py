3009a7fc98b82ae1f9a020dff98ecb72e5154367
Juan Sebastian Dussan Cubides <cubidesjuan@gmail.com>
2018-10-22 12:36:32 -0500

    generartor fix

--- generator.py
+++ generator.py
@@ -23,7 +23,7 @@ class BatchGenerator(Sequence):
         self.batch_size         = batch_size
         self.labels             = labels
         self.downsample         = downsample
-        self.max_box_per_image  = 800
+        self.max_box_per_image  = max_box_per_image
         self.min_net_size       = (min_net_size//self.downsample)*self.downsample
         self.max_net_size       = (max_net_size//self.downsample)*self.downsample
         self.shuffle            = shuffle
